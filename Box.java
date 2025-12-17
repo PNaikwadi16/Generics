cb0872333a2d5680392823ac92276f5cdea9305b
PNaikwadi16 <112852751+PNaikwadi16@users.noreply.github.com>
2025-12-17 17:05:54 +0530

    Update Box.java

--- Box.java
+++ Box.java
@@ -6,12 +6,12 @@ public class Box<T> {
         Box<String> box = new Box<>();
         box.value = "Hello";
 
-        // No casting needed
         String text = box.value;
 
         System.out.println(text);
     }
 }
+
 
 
 
